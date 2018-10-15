4b828f21f9af8f4a7bfdb1c6f915e0e42f881e20
Nikita Zamiatin <4406015@gmail.com>
2018-10-15 15:58:23 -0400

    Route and main page notFound and NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { Loading } from './components/Loading';
+import {Switch, Route} from 'react-router-dom'
+import NotFound from './components/NotFound';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Header />
+      <main className="container">
+      <Switch>
+        <Route   exact path="/" component={Loading}/>
+        <Route path="*" component={NotFound} />
+      </Switch>
+      {/* <Loading /> */}
+      </main>
       </div>
     );
   }
---
+++ src/components/Loading.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class Loading extends Component {
+  render() {
+    return (
+    <div>
+    <div className="jumbotron">
+  <h1 className="display-3">Are you ready for a job!</h1>
+  <p className="lead">Nick Zamiatin Front</p>
+  <hr className="my-4"/>
+  <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+  <p className="lead">
+    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+    </div>
+
+    )
+  }
+}
+
+export default Loading
